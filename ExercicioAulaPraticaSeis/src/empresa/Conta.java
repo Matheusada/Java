2e964c8bf85a474ea2a8b933ea7b8443343f14ad
Matheusada <Matheus.a.d.a@hotmail.com>
2024-04-09 19:46:01 -0300

    Commit dominado

--- ExercicioAulaPraticaSeis/src/empresa/Conta.java
+++ ExercicioAulaPraticaSeis/src/empresa/Conta.java
@@ -11,13 +11,24 @@ public class Conta {
  }
  
  void depositar(int valor){
-     saldo += valor;
-     
+     if(valor<0){
+         throw new RuntimeException("Valor invalido");
+     }
+     saldo -=valor;
  }
- void sacar(int valor){
+ void sacar(int valor) throws Exception{
+     if(valor>saldo){
+        throw new RuntimeException("Saldo insuficiente");
+    }
+    if(valor <0){
+        throw new RuntimeException("Valor invalido");
+    }   
      saldo -= valor;
  }
  void transferir(int valor){
+     if(valor<0){
+         throw new RuntimeException("Valor invalido");     
+     }
      saldo -= valor;
  }
  void info(){
--- ExercicioAulaPraticaSeis/src/empresa/Principal.java
+++ ExercicioAulaPraticaSeis/src/empresa/Principal.java
@@ -9,14 +9,24 @@ public class Principal {
         Conta c1  = new Conta("Matheus", 15000);
         Conta c2  = new Conta("Mario", 75000);
         
-        c1.info();
-        c2.info();
-        c1.depositar(300);
-        System.out.println("--------");//contribuição
-        c1.info();
-        c1.sacar(800);
-        c1.info();
-        
+       
+        try {
+            c1.info();
+            c2.info();
+            c1.depositar(300);
+            c1.info();
+            c1.sacar(800000);
+            c1.info();  
+        }
+        catch(Exception e){ // poderia ser qualquer no lugar da letra "e"
+            System.out.println("Ocorreu um erro! Coloque outro valor! ");
+            System.out.println(e.getMessage()); // sempre teremos getMessage() para retornar o erro 8impresso !
+        }
+        finally {
+            System.out.println("");
+            System.out.println("Encerrando Programa...");
+        }
+         
         
     }
     
